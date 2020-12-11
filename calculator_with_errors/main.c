f29f7c74a68eadcf648a71728fad0158825a2c13
sina shariati <sina.shariati@yahoo.com>
2020-12-11 22:53:39 +0330

    Refactor big if block in main.c to switch case

--- calculator_with_errors/main.c
+++ calculator_with_errors/main.c
@@ -7,8 +7,8 @@ int main() {
     double number1, number2, result;
     char operator;
 
-    printf("Choose operation to perform (+, -, *, /, %%, sin, cos, tan, ): ");
-    scanf(" %c", &operator);
+    printf("Choose operation to perform (+, -, *, /, %%, sin, cos, tan, :");
+    scanf("%c", &operator);
 
 
     printf("Enter first number:");
@@ -17,28 +17,36 @@ int main() {
     scanf("%lf", &number2);
 
 
-    if (operator == '+') {
-        result = number1 + number2;
-    } else if (operator == '-') {
-        result = number1 - number2;
-    } else if (operator == '*') {
-        result = number1 * number2;
-    } else if (operator == '/') {
-        if (number2 == 0) {
-            printf("invalid input! divide by zero");
-            return -1;
-        } else {
+    switch (operator) {
+        case '+':
+            result = number1 + number2;
+            break;
+        case '-':
+            result = number1 - number2;
+            break;
+        case '*':
+            result = number1 * number2;
+            break;
+        case '/':
+            if (number2 == 0) {
+                printf("invalid input! divide by zero");
+                return -1;
+            }
             result = number1 / number2;
-        }
-    } else if (operator == '%') {
-        result = (int) number1 % (int) number2;
-    } else if (operator == 's') {
-        result = sin(number1 / number2);
-    } else if (operator == 'c') {
-        result = cos(number1 / number2);
-    } else if (operator == 't') {
-        result = tan(number1 / number2);
+            break;
+        case '%':
+            result = (int) number1 % (int) number2;
+            break;
+        case 's' :
+            result = sin(number1 / number2);
+            break;
+        case 'c' :
+            result = cos(number1 / number2);
+            break;
+        case 't' :
+            result = tan(number1 / number2);
     }
+
 
     if (operator == 's') {
         printf("result : sin(%f/%f) = %lf", number1, number2, result);
