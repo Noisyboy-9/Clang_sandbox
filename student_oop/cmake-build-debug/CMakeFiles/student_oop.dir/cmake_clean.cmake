f0d3c0cf18886fef7c2de03910867421803f9185
sina shariati <sina.shariati@yahoo.com>
2021-01-13 22:31:13 +0330

    Finish add student feature of class_oop

---
+++ student_oop/cmake-build-debug/CMakeFiles/student_oop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/student_oop.dir/main.c.o"
+  "student_oop"
+  "student_oop.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/student_oop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
